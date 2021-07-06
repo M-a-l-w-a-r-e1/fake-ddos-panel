0ae51b14b10112176c5936bb58ddd27af6cc85e5
rose-is-gay <71583406+rose-is-gay@users.noreply.github.com>
2021-07-06 15:56:27 -0400

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ddos_panel_not_rat
+{
+    public partial class Form1 : Form
+    {
+        private const int EM_SETCUEBANNER = 0x1501;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern Int32 SendMessage(IntPtr hWnd, int msg, int wParam, [MarshalAs(UnmanagedType.LPWStr)]string lParam);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SendMessage(textBox1.Handle, EM_SETCUEBANNER, 0, "Username");
+            SendMessage(textBox2.Handle, EM_SETCUEBANNER, 0, "Password");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            panel p = new panel();
+            p.ShowDialog();
+        }
+    }
+}
---
+++ panel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ddos_panel_not_rat
+{
+    public partial class panel : Form
+    {
+        public panel()
+        {
+            InitializeComponent();
+        }
+
+        private void panel_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Please Launch Again", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
